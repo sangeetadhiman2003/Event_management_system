fe05a48b34921012ad4e95c8665dfae7f27c767e
sangeetadhiman <sangeetadhiman2003@gmail.com>
2024-09-14 17:12:50 +0530

    Added some privacy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
-end
+    before_action :authenticate_user!
+  
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  
+    protected
+  
+    def after_sign_in_path_for(resource)
+      events_path 
+    end
+  
+    def after_sign_out_path_for(resource_or_scope)
+      root_path
+    end
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:user_type])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:user_type])
+    end
+  end
+  
